327f83970a1c7ea21b79fcd961dc8d683a885098
Carlos Queiroz <carlostgti@outlook.com>
2019-07-12 14:20:25 -0300

    primeiro commit

---
+++ src/app.js
@@ -0,0 +1,18 @@
+import express from "express";
+import routes from "./routes";
+
+class App {
+  constructor() {
+    this.server = express();
+    this.middleares();
+    this.routes();
+  }
+  middleares() {
+    this.server.use(express.json());
+  }
+  routes() {
+    this.server.use(routes);
+  }
+}
+
+export default new App().server;
