ea2466e36452db769e14c88b12540a77110252cf
Carlos Gonçalves Queiroz <carlostgti@outlook.com>
2019-08-15 00:22:47 -0300

    Alterações mac

--- src/config/database.js
+++ src/config/database.js
@@ -1,10 +1,10 @@
 module.exports = {
   dialect: 'postgres',
-  host: 'localhost',
+  host: 'http://192.168.99.100',
   username: 'postgres',
   password: 'docker',
   database: 'gobarber',
-  port: 5433,
+  port: 5432,
   define: {
     timestamps: true,
     underscored: true,
